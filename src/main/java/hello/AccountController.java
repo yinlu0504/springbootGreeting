780bfd5ebd3a5fa835716942bcbffd19c62f3a40
yinlu0504 <yinlu0504@gmail.com>
2018-06-27 21:20:57 -0700

    add request method

--- src/main/java/hello/AccountController.java
+++ src/main/java/hello/AccountController.java
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
     private final Account lukeAccount = new Account("Luke",10);
     
-    @RequestMapping("/account")
+    @RequestMapping("/account", method = RequestMethod.GET)
     public Account account() {
         return lukeAccount;
     }
 
-    @RequestMapping("/deposit")
+    @RequestMapping("/deposit", method = RequestMethod.POST)
     public Account deposit(@RequestParam(value="amount",defaultValue="0") long amount ){
         lukeAccount.deposite(amount);
         return lukeAccount;
     }
 
-    @RequestMapping("/withdraw")
+    @RequestMapping("/withdraw", method = RequestMethod.POST)
     public Account withdraw(@RequestParam(value="amount",defaultValue="0") long amount ){
         lukeAccount.withdraw(amount);
         return lukeAccount;
