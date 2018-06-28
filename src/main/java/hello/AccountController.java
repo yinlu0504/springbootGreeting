c1cff55b6d960da6b1ede5ae21608e9d0a2f586d
Lu Yin <lyin@secdev.local>
2018-06-28 10:16:09 -0700

    try request method

--- src/main/java/hello/AccountController.java
+++ src/main/java/hello/AccountController.java
@@ -3,24 +3,30 @@ package hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
 
 @RestController
 
 public class AccountController {
     private final Account lukeAccount = new Account("Luke",10);
     
-    @RequestMapping("/account", method = RequestMethod.GET)
+    @RequestMapping(value = "/account")
+    @ResponseBody
     public Account account() {
         return lukeAccount;
     }
 
-    @RequestMapping("/deposit", method = RequestMethod.POST)
+    @RequestMapping(value = "/deposit")
     public Account deposit(@RequestParam(value="amount",defaultValue="0") long amount ){
         lukeAccount.deposite(amount);
         return lukeAccount;
     }
 
-    @RequestMapping("/withdraw", method = RequestMethod.POST)
+    @RequestMapping(value = "/withdraw")
+    @ResponseStatus(HttpStatus.OK)
     public Account withdraw(@RequestParam(value="amount",defaultValue="0") long amount ){
         lukeAccount.withdraw(amount);
         return lukeAccount;
